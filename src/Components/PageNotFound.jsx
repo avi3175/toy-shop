8cad80595379f9b411d4ff0080b33a74418cd6cb
avi3175 <avishahriar@.com>
2023-05-20 14:00:48 +0600

    11th fixed

--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -7,8 +7,8 @@ const PageNotFound = () => {
             <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
                 <div className="max-w-md mx-auto">
                     <img src="https://cdn.dribbble.com/users/1281023/screenshots/5984585/404_animation_render.gif" alt="404 Not Found" className="w-full" />
-                    <h1 className="text-4xl text-gray-800 font-bold mt-8">Oops! Page Not Found</h1>
-                    <p className="text-gray-600 mt-4">The page you are looking for does not exist.</p>
+                    <h1 className="text-4xl text-gray-800 font-bold mt-8 animate-bounce">Oops! Page Not Found</h1>
+                
                 </div>
             </div>
         </div>
