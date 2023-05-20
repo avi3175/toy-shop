a7eb4a2c7fc23a0371a3348cb487513b56b2cd00
avi3175 <avishahriar@.com>
2023-05-20 15:53:14 +0600

    17th

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,12 +5,12 @@ import toy3 from '../../public/image/car-gallery-03.jpg'
 import toy4 from '../../public/image/logic.png'
 const Gallery = () => {
   return (
-    <div className='mx-12'>
+    <div className='mx-10 mt-48'>
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <div className='flex items-center'>
             <p className='font-bold text-blue-500 text-6xl '>CAR TOYS</p>
-            <img src={toy1} className="w-full" />
+            <img src="https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/UUID-cl90hbhbn7185859tqy2wxtknym/public" className="w-[728px]" />
 
           </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -20,8 +20,8 @@ const Gallery = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <div className='flex items-center'>
-            <p className='font-bold text-blue-500 text-6xl '> PUZZLE TOYS</p>
-            <img src={toy4} className="w-full" />
+            <p className='font-bold text-blue-500 text-6xl '> TRAIN TOYS</p>
+            <img src='https://media.istockphoto.com/id/1196393819/photo/wooden-toy-railway-and-pyramid-in-the-children-room.jpg?b=1&s=170667a&w=0&k=20&c=grJyBZ_8OUPpvVgnqVdBWq7_ILjIAc05MlVBon9rzJk=' className="w-full" />
 
           </div>
 
@@ -33,7 +33,7 @@ const Gallery = () => {
         <div id="slide3" className="carousel-item relative w-full">
         <div className='flex items-center'>
             <p className='font-bold text-blue-500 text-6xl '>LEGO TOYS</p>
-            <img src={toy3} className="w-full" />
+            <img src='https://c4.wallpaperflare.com/wallpaper/468/700/853/car-lego-wallpaper-preview.jpg' className="w-full" />
 
           </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
